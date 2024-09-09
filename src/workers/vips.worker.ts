eb1d09df97e3853d745d65296921f9881cdd35ec
romanhrynevych <82209198+romanhrynevych@users.noreply.github.com>
2024-09-09 16:30:24 +0300

    Initial commit

---
+++ src/workers/vips.worker.ts
@@ -0,0 +1,39 @@
+// / <reference lib="webworker" />
+
+import { expose } from 'comlink';
+import onetime from 'onetime';
+import Vips, { type Blob } from 'wasm-vips';
+
+const getVips = onetime(async () =>
+  Vips({
+    // Optimize startup time by disabling the dynamic modules
+    dynamicLibraries: [],
+  })
+);
+
+async function getThumbnail(
+  file: Blob,
+  opt?: { width?: number; height?: number }
+): Promise<string> {
+  const { width = 320, height = 320 } = opt ?? {};
+
+  const vips = await getVips();
+
+  const im = vips.Image.thumbnailBuffer(file, width, {
+    height,
+    no_rotate: true,
+    crop: vips.Interesting.none,
+  });
+
+  const outBuffer = im.webpsaveBuffer({ Q: 80 });
+  const blob = new Blob([outBuffer], { type: `image/webp` });
+  return URL.createObjectURL(blob);
+}
+
+const workerInterface = {
+  getThumbnail,
+};
+
+expose(workerInterface);
+
+export type ExposedInterface = typeof workerInterface;
